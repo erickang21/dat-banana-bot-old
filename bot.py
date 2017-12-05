db67a867e1ec39a935dd7a399c4b5e3371d7fb5b
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-05 16:02:05 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,87 @@
+import discord
+import os
+import io
+import textwrap
+from contextlib import redirect_stdout
+import traceback
+from discord.ext import commands 
+bot = commands.Bot(command_prefix='*',description="It's a something bot made by dat banana boi #1982.\n\nHelp Commands",owner_id=277981712989028353)
+bot.load_extension("cogs.banana")
+
+
+def cleanup_code(content):
+    """Automatically removes code blocks from the code."""
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+
+    return content.strip('` \n')
+    
+    
+@bot.event
+async def on_ready():
+   print('Bot is online!') 
+   
+   
+@bot.command()
+async def readycheck(ctx):
+    """I dare you to guess. I mean, like, to see if I'm working."""
+    await ctx.send("You were wrong! Looks like I AM working!")
+    
+    
+    
+    
+    
+@bot.command(pass_context=True, hidden=True, name='eval')
+@commands.is_owner()
+async def _eval(ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+        }
+
+        env.update(globals())
+
+        body = cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                await ctx.send(f'```py\n{value}{ret}\n```')
+   
+
+if not os.environ.get('TOKEN'):
+    print("no token found REEEE!")
+bot.run(os.environ.get('TOKEN').strip('"'))
+   
+   
