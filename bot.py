f68f62e934f6b51a0dee5cee8f6a17bc1c0bd7e2
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-05 16:08:25 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,9 +29,15 @@ async def readycheck(ctx):
     await ctx.send("You were wrong! Looks like I AM working!")
     
     
-    
-    
-    
+@commands.command()
+    async def ping(ctx):
+        """Websocket latency, delivered through premium ping pong."""
+        em = discord.Embed(color=discord.Color(value=0x00ff00))
+        em.title = "PoIIIIng! That took:"
+        em.description = f'{bot.ws.latency * 1000:.4f} ms'
+        await ctx.send(embed=em)
+        
+        
 @bot.command(pass_context=True, hidden=True, name='eval')
 @commands.is_owner()
 async def _eval(ctx, *, body: str):
