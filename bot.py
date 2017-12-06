eba42bfc417ac094806bc8d5c864411d8a14a3b1
dogoo <regmibatshal@gmail.com>
2017-12-06 17:07:08 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,52 +125,50 @@ async def discord(ctx):
     await ctx.send("Your turn to enter the hood. https://discord.gg/wvkVknA")
         
         
-@bot.command(pass_context=True, hidden=True, name='eval')
+@bot.command(hidden=True, name='eval')
 @commands.is_owner()
 async def _eval(ctx, *, body: str):
-        """Evaluates a code"""
 
-        env = {
-            'bot': bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-        }
+    env = {
+        'bot': bot,
+        'ctx': ctx,
+        'channel': ctx.channel,
+        'author': ctx.author,
+        'guild': ctx.guild,
+        'message': ctx.message,
+    }
 
-        env.update(globals())
+    env.update(globals())
 
-        body = cleanup_code(body)
-        stdout = io.StringIO()
+    body = cleanup_code(body)
+    stdout = io.StringIO()
 
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
 
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
 
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+    else:
+        value = stdout.getvalue()
         try:
-            exec(to_compile, env)
-        except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            await ctx.message.add_reaction('\u2705')
+        except:
+            pass
 
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        if ret is None:
+            if value:
+                await ctx.send(f'```py\n{value}\n```')
         else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                await ctx.send(f'```py\n{value}{ret}\n```')
+            await ctx.send(f'```py\n{value}{ret}\n```')    
    
 
 if not os.environ.get('TOKEN'):
