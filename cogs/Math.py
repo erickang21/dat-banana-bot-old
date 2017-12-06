042a011eb0178fb202630a3b0f3cd8057b822154
dogoo <regmibatshal@gmail.com>
2017-12-05 21:55:59 -0500

    Update Math.py

--- cogs/Math.py
+++ cogs/Math.py
@@ -2,6 +2,8 @@ import discord
 import sys
 import os
 import io
+import math
+import operator
 from discord.ext import commands
 
 
@@ -10,8 +12,8 @@ class Math:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def add(ctx, self, num: int, num2: int):
+@commands.command()
+async def add(ctx, self, num: int, num2: int):
         '''It...ADDS? Yea. Usage: *add [no.1] [no.2]'''
         if num is None:
             await ctx.send("Aren't you stupid enough? Usage: *add [no.1] [no.2]")
@@ -19,8 +21,8 @@ class Math:
             await ctx.send(num + num2)
         
         
-    @commands.command()
-    async def subtract(ctx, self, num: int, num2: int):
+@commands.command()
+async def subtract(ctx, self, num: int, num2: int):
         '''It...SUBTRACTS? Yea. Usage: *subtract [no.1] [no.2]'''
         if num is None:
             await ctx.send("Aren't you stupid enough? Usage: *subtract [no.1] [no.2]")
@@ -28,8 +30,8 @@ class Math:
             await ctx.send(num - num2)
         
         
-    @commands.command()
-    async def multiply (ctx, self, num: int, num2: int):
+@commands.command()
+async def multiply (ctx, self, num: int, num2: int):
         '''It...MULTIPLIES? Yea. Usage: *multiply [no.1] [no.2]'''
         if num is None:
             await ctx.send("Aren't you stupid enough? Usage: *multiply [no.1] [no.2]")
@@ -37,8 +39,8 @@ class Math:
             await ctx.send(num * num2)
         
         
-    @commands.command()
-    async def divide (ctx, self, num: int, num2: int):
+@commands.command()
+async def divide (ctx, self, num: int, num2: int):
         '''It...DIVIDES? Yea. Usage: *divide [no.1] [no.2]'''
         if num is None:
             await ctx.send("Aren't you stupid enough? Usage: *divide [no.1] [no.2]")
