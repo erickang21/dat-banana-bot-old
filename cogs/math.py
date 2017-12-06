307057bd58d78eed0fef4627a1c55ec0c5c189b0
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-06 15:34:35 -0500

    Update math.py

--- cogs/math.py
+++ cogs/math.py
@@ -2,14 +2,13 @@ import discord
 import sys
 import os
 import io
-import math
-import operator
 from discord.ext import commands
 
 
 class Math:
     def __init__(self, bot):
         self.bot = bot
+
 
 @commands.command()
 async def add(ctx, self, num: int, num2: int):
