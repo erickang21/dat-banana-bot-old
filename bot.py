596c415849c9add68d70d854641022998db5f694
dogoo <regmibatshal@gmail.com>
2017-12-07 16:57:41 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ from discord.ext import commands
 import json
 bot = commands.Bot(command_prefix='*',description="It's a something bot made by dat banana boi #1982.\n\nHelp Commands",owner_id=277981712989028353)
 bot.load_extension("cogs.math")
-bot.load_extension("cogs.Errorhandler")
 bot.load_extension("cogs.mod")
 bot.load_extension("cogs.utility")
 
