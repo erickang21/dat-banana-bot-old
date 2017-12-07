1d7e67df5a451967462a915c2272a75001d72c20
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-07 16:27:02 -0500

    create to fix ping

---
+++ cogs/utility.py
@@ -0,0 +1,23 @@
+import discord
+import sys
+import os
+import io
+from discord.ext import commands
+
+
+class utility:
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+    @bot.command()
+    async def ping(ctx):
+        "Websocket latency, delivered through premium ping pong."""
+        em = discord.Embed(color=discord.Color(value=0x00ff00))
+        em.title = "PoIIIIng! That took:"
+        em.description = f'{bot.ws.latency * 1000:.4f} ms'
+        await ctx.send(embed=em)
+    
+    
+def setup(bot): 
+    bot.add_cog(utility(bot))           
