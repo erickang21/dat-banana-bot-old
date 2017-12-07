d4cdfdc24bda92e3ecce0609cfb6d0c83433cf36
dogoo <regmibatshal@gmail.com>
2017-12-07 18:11:32 -0500

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -35,6 +35,35 @@ class mod:
         """Swings the mighty Ban Hammer on that bad boy."""
         await ctx.channel.send(f"The ban hammer has fallen. And it has struck {user.name}.")
         await user.ban()
+        
+@commands.command()
+async def serverinfo(self, ctx):
+        """Displays Info About The Server!"""
+        guild = ctx.guild 
+        roles = [x.name for x in guild.roles] 
+        role_length = len(roles)
+        roles = ', '.join(roles)
+        channels = len(guild.channels)
+        time = str(guild.created_at.strftime("%b %m, %Y, %A, %I:%M %p")) 
+
+        embed = discord.Embed(description="-", title='Server Info', colour=passcolor)
+        if guild.icon_url:
+            embed.set_thumbnail(url=guild.icon_url)
+        else:
+            embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
+        embed.add_field(name='__Server __', value=str(guild.name))
+        embed.add_field(name='__Server ID__', value=str(guild.id))
+        embed.add_field(name='__Owner__', value=str(guild.owner))
+        embed.add_field(name='__Owner ID__', value=guild.owner_id) 
+        embed.add_field(name='__Member Count__', value=str(guild.member_count))
+        embed.add_field(name='__Text/Voice Channels__', value=str(channels))
+        embed.add_field(name='__Server Region__', value='%s' % str(guild.region))
+        embed.add_field(name='__ Total Roles__', value='%s' % str(role_length))
+        embed.add_field(name='__Roles__', value='%s' % str(roles))
+        embed.set_footer(text='Created - %s' % time)
+
+        await ctx.send(embed=embed)
+
             
             
 def setup(bot): 
